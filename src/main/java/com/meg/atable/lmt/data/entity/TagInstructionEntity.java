99aaefe57779c6b3180b3828b198b581dae4a695
ophielia <ophielia@yahoo.com>
2019-03-09 09:53:33 +0100

    bugfix: tag instructions weren't found.

--- src/main/java/com/meg/atable/lmt/data/entity/TagInstructionEntity.java
+++ src/main/java/com/meg/atable/lmt/data/entity/TagInstructionEntity.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
  * Created by margaretmartin on 08/12/2017.
  */
 @Entity
-@DiscriminatorValue("TAG")
+@DiscriminatorValue("Tag")
 public class TagInstructionEntity extends AutoTagInstructionEntity implements Instruction {
     @Transient
     private Set<Long> inflatedSearchTerms;
