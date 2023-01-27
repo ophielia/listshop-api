8411b90d9d0fe0c3ba1a69b1c948a0e85d91b7ec
margaretmartin <ophielia@yahoo.com>
2023-01-27 22:40:56 +0100

    instrumentation - init container

--- listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
+++ listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
@@ -3,12 +3,14 @@
 
 ls /opt/listshop
 echo 'starting copy files'
+ls staticfiles
+echo 'starting copy files'
 cd /opt/listshop
 mkdir glowroot
 mkdir static
 cd ../..
 echo 'made directory, beginning to copy glowroot'
-cp  ./staticfiles/glowroot/* /opt/listshop/glowroot
+cp  -rf ./staticfiles/glowroot/* /opt/listshop/glowroot
 echo 'copied glowroot, beginning to copy json files'
 cp ./staticfiles/*.json /opt/listshop/static
 echo 'copied json files, beginning to copy png files'
