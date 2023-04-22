50a7fbc441a4815a30beaeb707b900d83c88db18
margaretmartin <ophielia@yahoo.com>
2023-04-22 22:33:36 +0200

    LX-1672 - change configuration for static files.  Remove static file serving for container(prod)

--- listshop-service/src/main/java/com/meg/listshop/configuration/StaticResourceConfiguration.java
+++ listshop-service/src/main/java/com/meg/listshop/configuration/StaticResourceConfiguration.java
@@ -2,6 +2,7 @@ package com.meg.listshop.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Created by margaretmartin on 04/10/2017.
  */
 @Configuration
+@Profile("!container")
 public class StaticResourceConfiguration implements WebMvcConfigurer {
 
     @Value("${listshop.static.resources.location}")
