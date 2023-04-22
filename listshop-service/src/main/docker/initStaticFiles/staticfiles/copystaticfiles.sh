9c7a817b22ad0a45c0655f00bb0fe972c0dcf603
margaretmartin <ophielia@yahoo.com>
2023-04-22 17:32:49 +0200

    changing static files init for new component.

--- listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
+++ listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
@@ -6,4 +6,6 @@ echo 'Beginning to copy json files'
 cp ./staticfiles/*.json /opt/listshop/static
 echo 'Beginning to copy png files'
 cp ./staticfiles/*.png /opt/listshop/static
+echo 'Beginning to copy html files'
+cp ./staticfiles/*.html /opt/listshop/static
 echo 'Done!'
