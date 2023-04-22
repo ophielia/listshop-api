b5c663e540dd04d9f43e75d67c252ee460753c32
margaretmartin <ophielia@yahoo.com>
2023-04-22 17:01:26 +0200

    changing static files init for new component.

--- listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
+++ listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
@@ -2,14 +2,8 @@
 
 
 echo 'starting copy files'
-cd /opt/listshop
-mkdir glowroot
-mkdir static
-cd ../..
-echo 'made directory, beginning to copy glowroot'
-cp  -rf ./staticfiles/glowroot/* /opt/listshop/glowroot
-echo 'copied glowroot, beginning to copy json files'
+echo 'Beginning to copy json files'
 cp ./staticfiles/*.json /opt/listshop/static
-echo 'copied json files, beginning to copy png files'
+echo 'Beginning to copy png files'
 cp ./staticfiles/*.png /opt/listshop/static
 echo 'Done!'
