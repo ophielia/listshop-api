118294bc4528844aeb3cb8a5727fcec2ed4676ff
margaretmartin <ophielia@yahoo.com>
2023-04-24 07:33:58 +0200

    LS-1676 - move static image files to their own directory

--- listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
+++ listshop-service/src/main/docker/initStaticFiles/staticfiles/copystaticfiles.sh
@@ -4,8 +4,12 @@
 echo 'starting copy files'
 echo 'Beginning to copy json files'
 cp ./staticfiles/*.json /opt/listshop/static
-echo 'Beginning to copy png files'
-cp ./staticfiles/*.png /opt/listshop/static
+echo 'Making directory for images'
+cd /opt/listshop
+mkdir images
+cd ../..
+echo 'Beginning to copy image files'
+cp ./staticfiles/*.png /opt/listshop/static/images
 echo 'Beginning to copy html files'
 cp ./staticfiles/*.html /opt/listshop/static
 echo 'Done!'
