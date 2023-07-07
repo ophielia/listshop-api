2ca3f2a715723ed7da41ec7bcb98bb0b1434b1cd
margaretmartin <ophielia@yahoo.com>
2023-07-07 21:45:29 +0200

    LS-1824 - adding screenshot user

--- listshop-service/src/main/java/com/meg/listshop/lmt/service/task/UserMetricsTask.java
+++ listshop-service/src/main/java/com/meg/listshop/lmt/service/task/UserMetricsTask.java
@@ -10,7 +10,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by margaretmartin on 30/10/2017.
@@ -24,16 +26,65 @@ public class UserMetricsTask {
     @Value("${web.login.period.in.days:8}")
     int deleteLoginsOlderThan;
 
+    private final AtomicInteger usersTotal;
+    private final AtomicInteger usersLoggedInWebLastDay;
+    private final AtomicInteger usersLoggedInWebLastWeek;
+    private final AtomicInteger usersLoggedInWebLastMonth;
+    private final AtomicInteger usersLoggedInMobileLastDay;
+    private final AtomicInteger usersLoggedInMobileLastWeek;
+    private final AtomicInteger usersLoggedInMobileLastMonth;
+    private final AtomicInteger usersLoggedInLastDay;
+    private final AtomicInteger usersLoggedInLastWeek;
+    private final AtomicInteger usersLoggedInLastMonth;
+    private final AtomicInteger usersActiveLastDay;
+    private final AtomicInteger usersActiveLastWeek;
+    private final AtomicInteger usersActiveLastMonth;
 
     UserRepository userRepository;
 
     public static final String LAST_DAY_TAG = "last_day";
     public static final String LAST_WEEK_TAG = "last_week";
     public static final String LAST_MONTH_TAG = "last_month";
+    public static final String WEB = "web";
+    public static final String MOBILE = "mobile";
+    public static final String LOGGED_IN = "loggedIn";
+    public static final String ACTIVE = "active";
+
+    private final HashMap<String, AtomicInteger> metricLookup;
 
     @Autowired
     public UserMetricsTask(UserRepository userDeviceRepository) {
+
         this.userRepository = userDeviceRepository;
+
+        // create guages
+        usersTotal = Metrics.gauge(String.format("users.total"), new AtomicInteger());
+        usersLoggedInWebLastDay = Metrics.gauge(String.format("users.logged-in.web.last_day"), new AtomicInteger());
+        usersLoggedInWebLastWeek = Metrics.gauge(String.format("users.logged-in.web.last_week"), new AtomicInteger());
+        usersLoggedInWebLastMonth = Metrics.gauge(String.format("users.logged-in.web.last_month"), new AtomicInteger());
+        usersLoggedInMobileLastDay = Metrics.gauge(String.format("users.logged-in.mobile.last_day"), new AtomicInteger());
+        usersLoggedInMobileLastWeek = Metrics.gauge(String.format("users.logged-in.mobile.last_week"), new AtomicInteger());
+        usersLoggedInMobileLastMonth = Metrics.gauge(String.format("users.logged-in.mobile.last_month"), new AtomicInteger());
+        usersLoggedInLastDay = Metrics.gauge(String.format("users.logged-in.last_day"), new AtomicInteger());
+        usersLoggedInLastWeek = Metrics.gauge(String.format("users.logged-in.last_week"), new AtomicInteger());
+        usersLoggedInLastMonth = Metrics.gauge(String.format("users.logged-in.last_month"), new AtomicInteger());
+        usersActiveLastDay = Metrics.gauge(String.format("users.active.last_day"), new AtomicInteger());
+        usersActiveLastWeek = Metrics.gauge(String.format("users.active.last_week"), new AtomicInteger());
+        usersActiveLastMonth = Metrics.gauge(String.format("users.active.last_month"), new AtomicInteger());
+
+        metricLookup = new HashMap<>();
+        metricLookup.put(WEB + LAST_DAY_TAG, usersLoggedInWebLastDay);
+        metricLookup.put(WEB + LAST_WEEK_TAG, usersLoggedInWebLastWeek);
+        metricLookup.put(WEB + LAST_MONTH_TAG, usersLoggedInWebLastMonth);
+        metricLookup.put(MOBILE + LAST_DAY_TAG, usersLoggedInMobileLastDay);
+        metricLookup.put(MOBILE + LAST_WEEK_TAG, usersLoggedInMobileLastWeek);
+        metricLookup.put(MOBILE + LAST_MONTH_TAG, usersLoggedInMobileLastMonth);
+        metricLookup.put(LOGGED_IN + LAST_DAY_TAG, usersLoggedInLastDay);
+        metricLookup.put(LOGGED_IN + LAST_WEEK_TAG, usersLoggedInLastWeek);
+        metricLookup.put(LOGGED_IN + LAST_MONTH_TAG, usersLoggedInLastMonth);
+        metricLookup.put(ACTIVE + LAST_DAY_TAG, usersActiveLastDay);
+        metricLookup.put(ACTIVE + LAST_WEEK_TAG, usersActiveLastWeek);
+        metricLookup.put(ACTIVE + LAST_MONTH_TAG, usersActiveLastMonth);
     }
 
     @Scheduled(cron = "${metrics.cron.expression:0 0 */4 * * *}")
@@ -84,31 +135,34 @@ public class UserMetricsTask {
         logger.info("-- begin gatherStatsTotalUserCount ");
 
         Integer count = userRepository.getTotalUserCount();
-        var metricName = String.format("users.total");
-        Metrics.gauge(metricName, count);
+        usersTotal.set(count);
 
         logger.info("-- end gatherStatsTotalUserCount ");
     }
 
     private void publishLoggedInByClient(LocalDate date, String timeTag) {
         Map<String, Integer> loggedInByClient = userRepository.getLoggedInUserCountByClient(date);
-
-        loggedInByClient.forEach((key, value) -> {
-            var metricName = String.format("users.logged-in.%s.%s", key, timeTag);
-            Metrics.gauge(metricName, value);
-        });
+        logger.info("--- begin publishLoggedInByClient [{}][{}] ", date, timeTag);
+        loggedInByClient
+                .forEach((key, value) -> {
+                    var lookupName = key + timeTag;
+                    metricLookup.get(lookupName).set(value);
+                    logger.info("---- begin publishLoggedInByClient [{}][{}] ", lookupName, value);
+                });
     }
 
     private void publishLoggedIn(LocalDate date, String timeTag) {
         Integer count = userRepository.getLoggedInUserCount(date);
-        var metricName = String.format("users.logged-in.%s", timeTag);
-        Metrics.gauge(metricName, count);
+        var lookupName = LOGGED_IN + timeTag;
+        metricLookup.get(lookupName).set(count);
+        logger.info("---  publishLoggedIn [{}][{}][{}] ", date, timeTag, count);
     }
 
     private void publishActive(LocalDate date, String timeTag) {
         Integer count = userRepository.getActiveUserCount(date);
-        var metricName = String.format("users.active.%s", timeTag);
-        Metrics.gauge(metricName, count);
+        var lookupName = ACTIVE + timeTag;
+        metricLookup.get(lookupName).set(count);
+        logger.info("---  publishActive [{}][{}][{}] ", date, timeTag, count);
     }
 
 
--- listshop-service/src/main/resources/db/screenshot/migration/V5__testtweaks.sql
+++ listshop-service/src/main/resources/db/screenshot/migration/V5__testtweaks.sql
@@ -1,3 +1,35 @@
 update list
 set last_update = now()
 where name = 'Grocery Store';
+
+-- adjust rating tags for Lemony Pasta
+insert into public.dish_tags (dish_id, tag_id)
+values (53, 19),
+       (53, 24),
+       (53, 104),
+       (53, 109),
+       (53, 117),
+       (53, 131),
+       (53, 193),
+       (53, 303),
+       (53, 320),
+       (53, 334),
+       (53, 344),
+       (53, 348),
+       (53, 354),
+       (53, 360),
+       (53, 406),
+       (53, 218),
+       (53, 400),
+       (53, 346),
+       (53, 328),
+       (53, 395),
+       (53, 417),
+       (53, 426),
+       (53, 321);
+
+-- adding list "Corner Store"
+insert into public.list (list_id, created_on, user_id, list_types, list_layout_id, last_update, meal_plan_id,
+                         is_starter_list, name)
+values (50991, '2023-06-28 13:40:43.093000 +00:00', 34, null, null, '2023-06-28 15:40:57.794000', null, false,
+        'Corner Store');
