019eee7f612b60352401d7a56e750c8ff5c38146
margaretmartin <ophielia@yahoo.com>
2023-08-04 23:41:17 -0600

    static files

--- listshop-service/src/main/resources/db/screenshot/migration/V1__init.sql
+++ listshop-service/src/main/resources/db/screenshot/migration/V1__init.sql
@@ -994,3 +994,44 @@ create table if not exists public.user_properties
 alter table public.user_properties
     owner to postgres;
 
+CREATE TABLE if not exists public.campaigns
+(
+    campaign_id
+    bigint
+    NOT
+    NULL,
+    created_on
+    timestamp
+    without
+    time
+    zone,
+    email
+    character
+    varying
+(
+    255
+) COLLATE pg_catalog."default",
+    campaign character varying
+(
+    255
+) COLLATE pg_catalog."default",
+    user_id bigint,
+    CONSTRAINT campaign_pkey PRIMARY KEY
+(
+    campaign_id
+)
+    )
+    WITH (
+        OIDS = FALSE
+        );
+
+
+CREATE SEQUENCE if not exists public.campaign_sequence
+    INCREMENT 1
+    START 1000
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+alter table public.campaigns
+    owner to postgres;
