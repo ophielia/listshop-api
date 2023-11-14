1a8738a3ab21eb79406e594c20d154604098f694
margaretmartin <ophielia@yahoo.com>
2023-11-14 06:31:23 +0100

    LS-1964 - beginning tests

---
+++ conversion/conversion-service/src/test/java/com/meg/listshop/conversion/service/MockConversionServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.meg.listshop.conversion.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MockConversionServiceImplTest {
+
+
+    @Test
+    void whenCallingSayHello_thenReturnHello() {
+        assertTrue("Hello".equals("Hello"));
+    }
+}
