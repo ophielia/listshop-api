50e2eb7c2e92fae4412dc63202ea2ce8048353ab
margaretmartin <ophielia@yahoo.com>
2023-11-22 07:47:13 +0100

    LS-1964 -  test (not working)

--- listshop-lmt/listshop-service/src/test/java/com/meg/listshop/lmt/service/ConversionTest.java
+++ listshop-lmt/listshop-service/src/test/java/com/meg/listshop/lmt/service/ConversionTest.java
@@ -9,20 +9,24 @@ package com.meg.listshop.lmt.service;
 
 import com.meg.listshop.Application;
 import com.meg.listshop.configuration.ListShopPostgresqlContainer;
+import com.meg.listshop.conversion.data.entity.UnitEntity;
 import com.meg.listshop.conversion.data.pojo.SimpleAmount;
+import com.meg.listshop.conversion.data.repository.UnitRepository;
+import com.meg.listshop.conversion.exceptions.ConversionFactorException;
+import com.meg.listshop.conversion.exceptions.ConversionPathException;
+import com.meg.listshop.conversion.service.ConversionService;
 import com.meg.listshop.conversion.service.ConvertibleAmount;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -40,13 +44,13 @@ public class ConversionTest {
     public static ListShopPostgresqlContainer postgreSQLContainer = ListShopPostgresqlContainer.getInstance();
 
     @Test
-    public void blowUpTest() {
-        Unit
-        ConvertibleAmount amount = new SimpleAmount(1, metricGrams);
-        ConvertibleAmount converted = conversionService.convert(amount, imperialOunces);
+    public void blowUpTest() throws ConversionPathException, ConversionFactorException {
+        Optional<UnitEntity> gramUnitOpt = unitRepository.findById(1013L);
+        Optional<UnitEntity> ozUnitOpt = unitRepository.findById(1009L);
+        ConvertibleAmount amount = new SimpleAmount(1, gramUnitOpt.get());
+        ConvertibleAmount converted = conversionService.convert(amount, ozUnitOpt.get());
         assertNotNull(converted);
         assertEquals(0.5, converted.getQuantity());
 
-        conversionService.processEmail(parameters);
     }
 }
