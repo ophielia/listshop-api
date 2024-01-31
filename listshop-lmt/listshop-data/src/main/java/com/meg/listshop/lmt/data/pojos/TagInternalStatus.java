059705baee083f5b2f30e0b6c133e007470ac179
margaretmartin <ophielia@yahoo.com>
2024-01-31 06:00:42 +0100

    LS-2014 - adding category_assigned to TagInternalStatus.

--- listshop-lmt/listshop-data/src/main/java/com/meg/listshop/lmt/data/pojos/TagInternalStatus.java
+++ listshop-lmt/listshop-data/src/main/java/com/meg/listshop/lmt/data/pojos/TagInternalStatus.java
@@ -5,7 +5,9 @@ public enum TagInternalStatus {
     CHECKED(3),
     LIQUID_ASSIGNED(5),
     FOOD_ASSIGNED(7),
-    FOOD_VERIFIED(11);
+    FOOD_VERIFIED(11),
+
+    CATEGORY_ASSIGNED(13);
 
     private int value;
 
