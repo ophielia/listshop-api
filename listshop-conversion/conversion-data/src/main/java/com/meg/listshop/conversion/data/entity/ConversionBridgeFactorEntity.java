6866a2249c92a02fc8e83777e09ed73633a3e4fa
M Martin <ophielia@yahoo.com>
2026-05-02 18:39:37 +0200

    LS-2394 - bridge view and entity created.

---
+++ listshop-conversion/conversion-data/src/main/java/com/meg/listshop/conversion/data/entity/ConversionBridgeFactorEntity.java
@@ -0,0 +1,133 @@
+/*
+ * The List Shop
+ *
+ * Copyright (c) 2026.
+ */
+
+package com.meg.listshop.conversion.data.entity;
+
+import com.meg.listshop.common.data.entity.UnitEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bridge_factors")
+public class ConversionBridgeFactorEntity implements ConversionFactor {
+    @Id
+    @Column(name = "FACTOR_ID")
+    private Long factorId;
+
+    private Double factor;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "toUnit")
+    private UnitEntity toUnit;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "fromUnit")
+    private UnitEntity fromUnit;
+
+    @Column(name = "CONVERSION_ID")
+    private Long conversionId;
+
+    @Column(name = "reference_id")
+    private Long referenceId;
+
+    private String marker;
+
+    @Column(name = "unit_size")
+    private String unitSize;
+
+    @Column(name = "unit_default")
+    private Boolean unitDefault;
+
+    public ConversionBridgeFactorEntity() {
+        // empty constructor for jpa
+    }
+
+    public Long getFactorId() {
+        return factorId;
+    }
+
+    public void setFactorId(Long factorId) {
+        this.factorId = factorId;
+    }
+
+    public Double getFactor() {
+        return factor;
+    }
+
+    public void setFactor(Double factor) {
+        this.factor = factor;
+    }
+
+    public UnitEntity getToUnit() {
+        return toUnit;
+    }
+
+    public void setToUnit(UnitEntity toUnit) {
+        this.toUnit = toUnit;
+    }
+
+    public UnitEntity getFromUnit() {
+        return fromUnit;
+    }
+
+    public void setFromUnit(UnitEntity fromUnit) {
+        this.fromUnit = fromUnit;
+    }
+
+    public Long getConversionId() {
+        return conversionId;
+    }
+
+    public void setConversionId(Long tagId) {
+        this.conversionId = tagId;
+    }
+
+    public Long getReferenceId() {
+        return referenceId;
+    }
+
+    public void setReferenceId(Long referenceId) {
+        this.referenceId = referenceId;
+    }
+
+    public String getMarker() {
+        return marker;
+    }
+
+    public void setMarker(String marker) {
+        this.marker = marker;
+    }
+
+    public String getUnitSize() {
+        return unitSize;
+    }
+
+    public void setUnitSize(String unitSize) {
+        this.unitSize = unitSize;
+    }
+
+    public Boolean isUnitDefault() {
+        return unitDefault != null && unitDefault;
+    }
+
+    public void setUnitDefault(Boolean unitDefault) {
+        this.unitDefault = unitDefault;
+    }
+
+    @Override
+    public String toString() {
+        return "ConversionFactorEntity{" +
+                "factorId=" + factorId +
+                ", factor=" + factor +
+                ", toUnit=" + toUnit +
+                ", fromUnit=" + fromUnit +
+                ", conversionId=" + conversionId +
+                ", referenceId=" + referenceId +
+                ", marker='" + marker + '\'' +
+                ", unitSize='" + unitSize + '\'' +
+                ", unitDefault=" + unitDefault +
+                '}';
+    }
+}
--- listshop-lmt/listshop-service/src/main/resources/maintenance/release/1.21.x/unit_defaults.sql
+++ listshop-lmt/listshop-service/src/main/resources/maintenance/release/1.21.x/unit_defaults.sql
@@ -4,11 +4,11 @@
  * Copyright (c) 2026.
  */
 
-update units set unit_default = false;
+update units set domain_defaul = false;
 
-update units set unit_default = TRUE where unit_id = 1003;
-update units set unit_default = TRUE where unit_id = 1007;
-update units set unit_default = TRUE where unit_id = 1009;
-update units set unit_default = TRUE where unit_id = 1013;
-update units set unit_default = TRUE where unit_id = 1026;
-update units set unit_default = TRUE where unit_id = 1059;
+update units set domain_defaul = TRUE where unit_id = 1003;
+update units set domain_defaul = TRUE where unit_id = 1007;
+update units set domain_defaul = TRUE where unit_id = 1009;
+update units set domain_defaul = TRUE where unit_id = 1013;
+update units set domain_defaul = TRUE where unit_id = 1026;
+update units set domain_defaul = TRUE where unit_id = 1059;
