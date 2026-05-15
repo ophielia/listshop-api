b35df186e33fd0867db69bf2ae4c30450108125b
M Martin <ophielia@yahoo.com>
2026-05-15 06:47:07 +0200

    LS-2394 - tag processor progress

--- listshop-lmt/listshop-service/src/main/resources/db/migration/standard/V3__add_domain_default.sql
+++ listshop-lmt/listshop-service/src/main/resources/db/migration/standard/V3__add_domain_default.sql
@@ -26,3 +26,27 @@ FROM factors f
          JOIN units b ON b.unit_id = f.from_unit
          JOIN factors t ON b.unit_id = t.from_unit
          JOIN units tou ON t.to_unit = tou.unit_id;
+
+create or replace view unit_factors
+            (factor, conversion_id, from_unit, from_name, from_marker, from_unit_size, from_unit_default, to_unit, to_name, to_marker, to_unit_size, to_unit_default)
+as
+select tou.unit_id * 1000000 + fr.unit_id AS factor_id,
+       f.factor / b.factor                as factor,
+       b.conversion_id                    AS conversion_id,
+       fr.unit_id                         AS from_unit,
+       fr.name                            AS from_name,
+       f.marker                           AS from_marker,
+       f.unit_size                        AS from_unit_size,
+       f.unit_default                     AS from_unit_default,
+       tou.unit_id                        AS to_unit,
+       tou.name                           AS to_name,
+       b.marker                           AS to_marker,
+       b.unit_size                        AS to_unit_size,
+       b.unit_default                     AS to_unit_default
+from factors f
+         join units fr on fr.unit_id = f.from_unit
+         JOIN factors b ON b.to_unit = f.to_unit and
+                           ((b.conversion_id = f.conversion_id) or
+                            (b.conversion_id is null and f.conversion_id is null))
+         join units tou on tou.unit_id = b.from_unit
+where b.conversion_id is not null;
