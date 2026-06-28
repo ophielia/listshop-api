ad43aa34424d28580ac120bd3881d1c48316e129
M Martin <ophielia@yahoo.com>
2026-06-28 18:41:30 +0200

    LS-2435 - updgrade scripts

--- listshop-lmt/listshop-service/src/test/resources/db/migration/test/V107__add_linked_layout_id.sql
+++ listshop-lmt/listshop-service/src/test/resources/db/migration/test/V107__add_linked_layout_id.sql
@@ -6,3 +6,19 @@
 
 ALTER TABLE list_layout ADD COLUMN linked_layout_id bigint;
 ALTER TABLE list_category ADD COLUMN linked_category_id bigint;
+
+update list_layout set linked_layout_id = 5;
+
+with base_layouts as (select *
+                      from list_category
+                      where layout_id = 5),
+     update_mapping as (select b.name as linked_id, b.category_id, us.name, us.category_id as to_update, ll.user_id, *
+                        from base_layouts b
+                                 join list_category us on us.name = b.name
+                                 join list_layout ll on ll.layout_id = us.layout_id
+                        where ll.layout_id <> 5
+                          and ll.user_id is not null)
+update list_category lc
+set linked_category_id = um.linked_id
+from update_mapping um
+where um.to_update = lc.category_id;
